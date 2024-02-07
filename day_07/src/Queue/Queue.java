42d9f9d75c327ce97dbc25e649b0ba9fd416c61a
sehaim <seha3722@gmail.com>
2024-02-07 12:25:46 +0900

    240207 day7 algorithm

---
+++ day_07/src/Queue/Queue.java
@@ -0,0 +1,58 @@
+package Queue;
+
+public class Queue {
+	static int[] queue = new int[] {};
+	static int front = -1;
+	static int rear = -1;
+	
+	public static void main(String[] args) {
+		createQueue(10);
+		for (int i=0; i<queue.length; i++) {
+			enQueue(i);
+		}
+		System.out.println(isFull());
+		
+		for (int i=0; i<queue.length; i++) {
+			int item = deQueue();
+			System.out.println(item);
+		}
+		System.out.println(isEmpty());
+		
+	}
+	
+	public static void createQueue(int size) {
+		queue = new int[size];
+	}
+	
+	public static void enQueue(int data) {
+		if (isFull()) {
+			System.out.println("데이터를 추가할 수 없습니다.");
+			return;
+		}
+		queue[++rear] = data;
+	}
+	
+	public static int deQueue() {
+		if(isEmpty()) {
+			System.out.println("더 이상 남아있는 데이터가 없습니다.");
+			return -1;
+		}
+		return queue[++front];
+	}
+	
+	public static int Qpeek() {
+		if(isEmpty()) {
+			System.out.println("더 이상 남아있는 데이터가 없습니다.");
+			return -1;
+		}
+		return queue[front + 1];
+	}
+	
+	public static boolean isFull() {
+		return rear == queue.length - 1;
+	}
+	
+	public static boolean isEmpty() {
+		return front == rear;
+	}
+}
---
+++ day_07/src/SWEA_1225_암호생성기/Solution.java
@@ -0,0 +1,68 @@
+package SWEA_1225_암호생성기;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static int[] pw = new int[8];
+	public static int pointer = -1;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = 1;
+		while (tc<=10) {
+			int T = sc.nextInt();
+			for(int i=0; i<pw.length; i++) {
+				pw[i] = sc.nextInt();
+			}
+			
+			int stop = 0;
+			int pointer2 = 0;
+			do {
+				for(int i=1; i<=5; i++) {
+					int bf = cycle(i);
+					if(isZero(bf)) {
+						pw[pointer] = 0;
+						pointer2 = pointer;
+						stop = 1;
+					}
+				}
+			} while (stop == 0);
+			
+			int[] newPW = swap(pointer2);
+			
+			System.out.print("#" + T + " ");
+			for (int i=0; i<newPW.length; i++) {
+				System.out.print(newPW[i] + " ");
+			}
+			
+			System.out.println();
+			tc++;
+		}
+		sc.close();
+	}
+	
+	
+	public static int cycle(int i) {
+		if(pointer == 7) {
+			pointer = -1;
+		}
+		return pw[++pointer] -= i;
+	}
+	
+	public static boolean isZero(int num) {
+		return num <= 0;
+	}
+	
+	public static int[] swap(int num) {
+		int[] arr = new int[8];
+		int j = 0;
+		for(int i=num+1; i<arr.length; i++) {
+			arr[j++] = pw[i];
+		}
+		for(int i=0; i<=num; i++) {
+			arr[j++] = pw[i];
+		}
+		
+		return arr;
+	}
+}
