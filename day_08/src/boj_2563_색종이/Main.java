58a62be6194441f9ef5c3e25da9f77eeff3e9d98
sehaim <seha3722@gamil.com>
2024-02-08 13:27:01 +0900

    240208 day8 algorithm

---
+++ day_08/src/boj_2563_색종이/Main.java
@@ -0,0 +1,41 @@
+package boj_2563_»öÁ¾ÀÌ;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int[][] arr = new int[100][100];
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int[] c_idx = new int[N];
+		int[] r_idx = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			c_idx[i] = sc.nextInt();
+			r_idx[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<N; i++) {
+			for(int c=c_idx[i]; c<c_idx[i]+10; c++) {
+				for(int r=90-r_idx[i]; r<100-r_idx[i]; r++) {
+					arr[r][c] = 1;
+				}
+			}
+		}
+		
+		int ans = 0;
+		
+		for(int r=0; r<100; r++) {
+			for(int c=0; c<100; c++) {
+				if(arr[r][c] == 1) {
+					ans += 1;
+				}
+			}
+		}
+		
+		System.out.println(ans);
+		sc.close();
+	}
+}
