13ef1d5ff2c5f174efe77877cffd7c1029a2b4a8
sehaim <seha3722@gmail.com>
2024-03-04 16:01:36 +0900

    240229 day16 algorithm

--- day_16/src/SWEA_7465_창용마을무리의개수/Solution.java
+++ day_16/src/SWEA_7465_창용마을무리의개수/Solution.java
@@ -7,13 +7,13 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		int x = 1;
-		
+
 		while (x <= T) {
 			int N = sc.nextInt();
 			int M = sc.nextInt();
-			int[] arr = new int[N+1];
-			
-			for(int i=0; i<N+1; i++) {
+			int[] arr = new int[N + 1];
+
+			for (int i = 0; i < N + 1; i++) {
 				arr[i] = i;
 			}
 
@@ -23,22 +23,22 @@ public class Solution {
 				int min = Math.min(arr[idx1], arr[idx2]);
 				int max = Math.max(arr[idx1], arr[idx2]);
 
-				for(int j=0; j<N+1; j++) {
-					if(arr[j] == max) {
+				for (int j = 0; j < N + 1; j++) {
+					if (arr[j] == max) {
 						arr[j] = min;
 					}
 				}
 			}
-			
-			int[] idxArr = new int[N+1];
+
+			int[] idxArr = new int[N + 1];
 			int num = 0;
-			
-			for(int i=1; i<N+1; i++) {
+
+			for (int i = 1; i < N + 1; i++) {
 				idxArr[arr[i]]++;
 			}
-			
-			for(int i=1; i<N+1; i++) {
-				if(idxArr[i] != 0) {
+
+			for (int i = 1; i < N + 1; i++) {
+				if (idxArr[i] != 0) {
 					num++;
 				}
 			}
