dafc87de11e460982463d594f17e0fba8de99ee2
sehaim <seha3722@gmail.com>
2024-03-04 15:51:26 +0900

    240229 day16 algorithm

--- day_16/src/SWEA_2806_NQueen/Solution.java
+++ day_16/src/SWEA_2806_NQueen/Solution.java
@@ -1,12 +1,11 @@
 package SWEA_2806_NQueen;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-	static int N, num, ans;
+	static int N, num;
 	static int[][] arr;
-	static int[] dr = { -1, -1, -1 };
+	static int[] dr = { 1, 1, 1 };
 	static int[] dc = { -1, 0, 1 };
 
 	public static void main(String[] args) {
@@ -17,12 +16,11 @@ public class Solution {
 		while (x <= T) {
 			N = sc.nextInt();
 			num = 0;
-			ans = 0;
 			arr = new int[N][N];
 
 			bfs(0);
 
-			System.out.println("#" + x + " " + ans);
+			System.out.println("#" + x + " " + num);
 
 			x++;
 		}
@@ -31,35 +29,30 @@ public class Solution {
 
 	public static void bfs(int r_idx) {
 		if (r_idx >= N) {
-			num = 0;
-			for(int r=0; r<N; r++) {
-				for(int c=0; c<N; c++) {
-					if(arr[r][c] == 2) num++;
-				}
-			}
-			if(num == N) ans++;
+			num++;
 			return;
 		}
 
 		for (int c = 0; c < N; c++) {
 			if (arr[r_idx][c] == 0) {
-				arr[r_idx][c] = 2;
+				arr[r_idx][c]++;
 				for (int d = 0; d < 3; d++) {
 					for (int j = 1; j < N; j++) {
 						int nr = r_idx + dr[d] * j;
 						int nc = c + dc[d] * j;
 						if (nr >= 0 && nr < N && nc >= 0 && nc < N) {
-							arr[nr][nc] = 1;
+							arr[nr][nc]++;
 						}
 					}
 				}
 				bfs(r_idx + 1);
+				arr[r_idx][c]--;
 				for (int d = 0; d < 3; d++) {
-					for (int j = 0; j < N; j++) {
+					for (int j = 1; j < N; j++) {
 						int nr = r_idx + dr[d] * j;
 						int nc = c + dc[d] * j;
 						if (nr >= 0 && nr < N && nc >= 0 && nc < N) {
-							arr[nr][nc] = 0;
+							arr[nr][nc]--;
 						}
 					}
 				}
--- day_16/src/SWEA_7465_창용마을무리의개수/Solution.java
+++ day_16/src/SWEA_7465_창용마을무리의개수/Solution.java
@@ -3,9 +3,6 @@ package SWEA_7465_창용마을무리의개수;
 import java.util.Scanner;
 
 public class Solution {
-	static int[] arr;
-	static int num;
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
@@ -14,26 +11,41 @@ public class Solution {
 		while (x <= T) {
 			int N = sc.nextInt();
 			int M = sc.nextInt();
-			num = 0;
-			arr = new int[N];
+			int[] arr = new int[N+1];
+			
+			for(int i=0; i<N+1; i++) {
+				arr[i] = i;
+			}
 
 			for (int i = 0; i < M; i++) {
-				bfs(sc.nextInt() - 1, sc.nextInt() - 1);
+				int idx1 = sc.nextInt();
+				int idx2 = sc.nextInt();
+				int min = Math.min(arr[idx1], arr[idx2]);
+				int max = Math.max(arr[idx1], arr[idx2]);
+
+				for(int j=0; j<N+1; j++) {
+					if(arr[j] == max) {
+						arr[j] = min;
+					}
+				}
+			}
+			
+			int[] idxArr = new int[N+1];
+			int num = 0;
+			
+			for(int i=1; i<N+1; i++) {
+				idxArr[arr[i]]++;
+			}
+			
+			for(int i=1; i<N+1; i++) {
+				if(idxArr[i] != 0) {
+					num++;
+				}
 			}
 
 			System.out.println("#" + x + " " + num);
 			x++;
 		}
 		sc.close();
-	}
-
-	public static void bfs(int idx1, int idx2) {
-		if (arr[idx1] == 0 && arr[idx2] == 0) {
-			arr[idx1] = arr[idx2] = ++num;
-		} else if (arr[idx1] == 0 && arr[idx2] != 0) {
-			arr[idx1] = arr[idx2];
-		} else if (arr[idx1] != 0 && arr[idx2] == 0) {
-			arr[idx2] = arr[idx1];
-		}
 	}
 }
