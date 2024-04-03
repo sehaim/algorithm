fb204724cee281f1effd62419f34369809f655d0
sehaim <seha3722@gmail.com>
2024-04-03 15:12:18 +0900

    240403 modified folder structure

--- day_19/src/SWEA_1251_하나로/Solution.java
+++
@@ -1,7 +0,0 @@
-package SWEA_1251_하나로;
-
-public class Solution {
-	public static void main(String[] args) {
-		
-	}
-}
--- day_19/src/SWEA_3289_서로소집합/Solution.java
+++
@@ -1,7 +0,0 @@
-package SWEA_3289_서로소집합;
-
-public class Solution {
-	public static void main(String[] args) {
-		
-	}
-}
