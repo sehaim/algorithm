7794efbf8b57dce7a08d2309e595b8b562d5af85
sehaim <seha3722@gmail.com>
2024-07-03 08:30:52 +0900

    boj_11054_가장긴바이토닉부분수열 tmp

---
+++ BOJ_Solution/src/boj_11054_가장긴바이토닉부분수열/Main.java
@@ -0,0 +1,22 @@
+package boj_11054_가장긴바이토닉부분수열;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+
+	}
+}
