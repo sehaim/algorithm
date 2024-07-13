b35f632f4efbd7f1adb43bebb116734844e18a0c
sehaim <seha3722@gmail.com>
2024-07-13 19:45:37 +0900

    boj_17386_선분교차 Main2.java 수정

--- BOJ_Solution/src/boj_17386_선분교차1/Main2.java
+++ BOJ_Solution/src/boj_17386_선분교차1/Main2.java
@@ -39,8 +39,13 @@ public class Main2 {
 		System.out.println(ans);
 
 	}
-
+	
+	// (x1, y1) -> (x2, y2), (x2, y2) -> (x3, y3) 로의 외적 계산
+	// 외적 결과가 음수이면 시계방향, 양수이면 시계 반대방향 
+	// ccw 내 매개 변수가 int형이면 정답 실패 -> 매개변수로 들어가는 좌표는 값의 범위가 정해져 있어서 return 값만 long 타입이면 될 것 같은데 아직 이유를 모르겠음.
 	public static int ccw(long x1, long y1, long x2, long y2, long x3, long y3) {
+		// 외적 결과도 마찬가지로 long 타입 매개변수로 받아도 정답 실패
+		// return 내에서 조건문으로 -1 또는 1로 반환해야 성공 
 		return (x2 - x1) * (y3 - y2) - (y2 - y1) * (x3 - x2) < 0 ? -1 : 1;
 	}
 }
