689e05aa64caf11f064010996db10513bc63ea44
sehaim <seha3722@gmail.com>
2024-12-22 01:51:36 +0900

    :memo: boj_17219_비밀번호찾기 README 및 코드 주석 추가

--- BOJ_Solution/src/boj_17219_비밀번호찾기/Main.java
+++ BOJ_Solution/src/boj_17219_비밀번호찾기/Main.java
@@ -14,14 +14,14 @@ public class Main {
 		Map<String, String> map = new HashMap<String, String>();
 
 		for (int i = 0; i < N; i++) {
-			String[] pwArr = br.readLine().split(" ");
+			String[] pwArr = br.readLine().split(" "); // 공백으로 사이트 주소와 비밀번호 구분하여 배열에 저장
 			map.put(pwArr[0], pwArr[1]);
 		}
 
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		for (int i = 0; i < M; i++) {
-			bw.write(map.get(br.readLine()) + "\n");
+			bw.write(map.get(br.readLine()) + "\n"); // 사이트 주소 입력받으며 비밀번호 찾기 
 		}
 
 		bw.flush();
---
+++ BOJ_Solution/src/boj_17219_비밀번호찾기/README.md
@@ -0,0 +1,9 @@
+## 📖 [비밀번호 찾기](https://www.acmicpc.net/problem/17219)
+<img src="./assets/17219_비밀번호찾기.png" width="600px" />
+
+---
+#### 📍 풀이
+- 기본적인 Map을 이용한 풀이
+---
+#### 📍 느낀점
+- 쉽다.ㅎㅎ
