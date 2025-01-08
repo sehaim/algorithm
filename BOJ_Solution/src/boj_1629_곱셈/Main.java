bcce0833860394dca20471d3f16a3bef6447b661
sehaim <seha3722@gmail.com>
2025-01-09 03:21:10 +0900

    :bulb:[241228] boj_1629_곱셈 문제 풀이 완료

--- BOJ_Solution/src/boj_1629_곱셈/Main.java
+++ BOJ_Solution/src/boj_1629_곱셈/Main.java
@@ -1,23 +1,35 @@
 package boj_1629_곱셈;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
+		long A = Integer.parseInt(st.nextToken());
+		long B = Integer.parseInt(st.nextToken());
+		long C = Integer.parseInt(st.nextToken());
+
+		// 결과 계산 및 출력
+		long result = power(A, B, C);
+
+		System.out.println(result);
 
 	}
 
-	public static int op(int A, int B, int C) {
-		int result = op(A, B / 2, C);
+	public static long power(long A, long B, long C) {
+		if (B == 1) {
+			return A % C;
+		}
+
+		long tmp = power(A, B / 2, C) % C;
+
 		if (B % 2 == 0) {
-			return result * result % C;
+			return tmp * tmp % C;
 		} else {
-			return result * result;
+			return tmp * tmp % C * A % C;
 		}
 	}
 }
