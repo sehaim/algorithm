3da7df6de19f259f21a6c42e507dc756cd6bac50
sehaim <seha3722@gmail.com>
2025-07-02 16:46:44 +0900

    :bulb:[250121] boj_9251_LCS 문제 풀이 완료

--- BOJ_Solution/src/boj_9251_LCS/Main.java
+++ BOJ_Solution/src/boj_9251_LCS/Main.java
@@ -1,50 +1,42 @@
 package boj_9251_LCS;
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int[] dp;
-    static int max;
-    static String str1, str2;
-    
-    public static void main(String[] args) throws IOException {
-        str1 = br.readLine();
-        str2 = br.readLine();
-        
-        dp = new int[str2.length()+1];
-        
-        for(int i = 0; i < str1.length(); i++){
-            dp(str1.charAt(i), 0, 0);
-        }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[] dp;
+	static int max, tmp_max;
+	static String str1, str2;
 
-        Arrays.sort(dp);
-        System.out.print(dp[str2.length()]);
-    }
-/*
-    static int lcs(String str1, String str2) {
+	public static void main(String[] args) throws IOException {
+		str1 = br.readLine();
+		str2 = br.readLine();
 
-        for (int i = 0; i < str1.length(); i++) {
-            int comm = 0;
-            for (int j = 0; j < str2.length(); j++) {
-                if (str1.charAt(i) == str2.charAt(j)) {
-                    if (comm < dp[j]) max = Math.max(max, comm = dp[j]);
-                    else max = Math.max(max, dp[j] = comm + 1);
-                } else max = Math.max(max, comm = Math.max(comm, dp[j]));
-            }
-        }
-        return max;
-    }
-*/
-    static int dp(char character, int lcs, int idx){
-        if(idx == str2.length()) return 0; // str2를 전부 탐색하면 return
+		dp = new int[str2.length() + 1];
+		max = 0; // 전체 최댓값
 
-        if(character == str2.charAt(idx) && lcs < dp[idx]) lcs = dp[idx]; // str1의 i번째 글자와 str2의 idx번째 글자가 같고, lcs가 dp[idx]보다 작으면 lcs = dp[idx]
-        else if(character == str2.charAt(idx))dp[idx] = lcs + 1;
-        else lcs = Math.max(lcs, dp[idx]);
+		// str1 순회하며 dp 실행
+		for (int i = 0; i < str1.length(); i++) {
+			dp(str1.charAt(i));
+		}
 
-        return dp(character, lcs , idx+1);
-    }
+		System.out.print(max); // 최댓값 출력
+	}
+
+	public static void dp(char character) {
+		tmp_max = 0; // dp 실행시 이전 index까지의 최댓값
+
+		for (int i = 0; i < str2.length(); i++) {
+			int tmp = dp[i]; // i번째에 저장되어 있는 dp값 임시 저장
+
+			if (str2.charAt(i) == character) { // 같은 문자 발견하면
+				tmp = Math.max(dp[i], tmp_max + 1); // 현재 dp 값과 (이전 index까지의 최댓값 + 1) 비교 후, 더 큰 값 tmp에 입력
+			}
+
+			tmp_max = Math.max(tmp_max, dp[i]); // 이전 index까지의 최댓값 갱신 (즉, i + 1 번째 실행 전에 i까지의 최댓값 저장)
+			dp[i] = tmp; // i까지의 최댓값 저장 후 dp[i]에 tmp값 입력
+			max = Math.max(max, dp[i]); // 전체 최댓값 갱신
+
+		}
+	}
 }
-
